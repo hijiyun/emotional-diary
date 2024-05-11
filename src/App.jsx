57b4536e2a9fe471942a36102070c6c6041e87e3
박지윤 <tkrhkfld123@naver.com>
2024-05-11 20:43:00 +0900

    setting: 폰트 및 아이콘 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 
+import emotion1 from "../public/assets/img/emotion1.png";
+import emotion2 from "../public/assets/img/emotion2.png";
+import emotion3 from "../public/assets/img/emotion3.png";
+import emotion4 from "../public/assets/img/emotion4.png";
+import emotion5 from "../public/assets/img/emotion5.png";
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
