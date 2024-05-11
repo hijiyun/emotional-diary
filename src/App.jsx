7f2d55bd145fef7424c9256f5f0bcb4d9d077cbb
박지윤 <tkrhkfld123@naver.com>
2024-05-11 20:53:11 +0900

    rename: 감정 이미지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 
-import emotion1 from "../public/assets/img/emotion1.png";
-import emotion2 from "../public/assets/img/emotion2.png";
-import emotion3 from "../public/assets/img/emotion3.png";
-import emotion4 from "../public/assets/img/emotion4.png";
-import emotion5 from "../public/assets/img/emotion5.png";
+import emotion1 from "./assets/img/emotion1.png";
+import emotion2 from "./assets/img/emotion2.png";
+import emotion3 from "./assets/img/emotion3.png";
+import emotion4 from "./assets/img/emotion4.png";
+import emotion5 from "./assets/img/emotion5.png";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary/:id" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <img src={emotion1} alt="" />
+        <img src={emotion2} alt="" />
+        <img src={emotion3} alt="" />
+        <img src={emotion4} alt="" />
+        <img src={emotion5} alt="" />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
