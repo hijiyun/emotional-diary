e87089eaff7e6f93f05f767e1ba08c5e5963c48e
박지윤 <tkrhkfld123@naver.com>
2024-05-12 16:36:55 +0900

    style: mock데이터 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ import { useReducer, useRef, createContext } from "react";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-05-12").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-05-11").getTime(),
     emotionId: 3,
     content: "2번 일기 내용",
   },
   {
     id: 3,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-04-19").getTime(),
     emotionId: 4,
     content: "3번 일기 내용",
   },
@@ -42,8 +42,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
