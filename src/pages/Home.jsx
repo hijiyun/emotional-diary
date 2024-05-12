affbfb0c7bf9b8b32cb07fff46fbf3f7f336db34
박지윤 <tkrhkfld123@naver.com>
2024-05-12 16:39:24 +0900

    style: 홈페이지 코드 작성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,54 @@
+import { useContext, useState } from "react";
+import { DiaryContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotData, data) => {
+  const beginTime = new Date(
+    pivotData.getFullYear(),
+    pivotData.getMonth(),
+    1,
+    0,
+    0,
+    0
+  );
+  const endTime = new Date(
+    pivotData.getFullYear(),
+    pivotData.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  );
+
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
+
 const Home = () => {
+  const [pivotData, setPvotDate] = useState(new Date());
+  const data = useContext(DiaryContext);
+
+  const monthlyData = getMonthlyData(pivotData, data);
+
+  const onIncreaseMonth = () => {
+    setPvotDate(new Date(pivotData.getFullYear(), pivotData.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPvotDate(new Date(pivotData.getFullYear(), pivotData.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 5월"}
-        leftChild={<Button>&lt;</Button>}
-        rightChild={<Button>&gt;</Button>}
+        title={`${pivotData.getFullYear()}년 ${pivotData.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth}>&lt;</Button>}
+        rightChild={<Button onClick={onIncreaseMonth}>&gt;</Button>}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 };
