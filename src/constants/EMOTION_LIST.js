6dcb5beabc1ae75844c8f353006c6f37ab722172
박지윤 <tkrhkfld123@naver.com>
2024-05-13 16:31:18 +0900

    chore: 이모션 이름 및 id 상수 설정

---
+++ src/constants/EMOTION_LIST.js
@@ -0,0 +1,22 @@
+export const EMOTION_LIST = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋아",
+  },
+  {
+    emotionId: 2,
+    emotionName: "조금 좋아",
+  },
+  {
+    emotionId: 3,
+    emotionName: "보통이야",
+  },
+  {
+    emotionId: 4,
+    emotionName: "조금 별로",
+  },
+  {
+    emotionId: 5,
+    emotionName: "완전 별로",
+  },
+];
